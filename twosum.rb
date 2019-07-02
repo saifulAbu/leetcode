6aa319d1d76bdbf6afeb7f2c0407ae162045bc3b
siddartha saif <saifulabu25@gmail.com>
2019-07-01 23:43:22 -0500

    adding more stuffs

--- twosum.rb
+++ twosum.rb
@@ -1,6 +1,12 @@
-ram {Integer[]} nums
+require 'set'
+
+# @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-    
+	hashmap = {}
+	nums.each_with_index do
+		|index, num|
+		hashmap[num] = index
+	end
 end
