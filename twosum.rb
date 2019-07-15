e8c4b2c7c07d3e4232ea1436ce9ee56e69581260
Saiful Abu <saifulabu25@gmail.com>
2019-07-15 08:00:14 -0500

    Update twosum.rb

--- twosum.rb
+++ twosum.rb
@@ -1,22 +1,22 @@
-require 'set'
-
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-	hashmap = {}
-	nums.each_with_index do
-		|index, num|
-		hashmap[num] = index
-	end
-	
-	nums.each_with_index do
-		|index, num|
-		puts nums[index]
-	end
+    val_map = {}
+    nums.each_with_index do
+        |v, i|
+        val_map[v] = i
+    end
+    
+    res = []
+    a = nums.each_with_index do
+        |v, i|
+        num2 = target - v
+        puts "#{v}, #{num2}"
+        if !val_map[num2].nil?
+            i2 = val_map[num2]
+            res = [i, i2]
+            return
+        end
+    end
 end
-
-two_sum [6, 7, 8, 9], 5
-
-
-
