85cb3c56084f943e73a23213ca7ddf39121faae6
Saif Abu <saif.abu@cerner.com>
2020-02-16 08:53:41 -0600

    adding two sum

--- twosum.rb
+++ twosum.rb
@@ -1,22 +1,36 @@
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
+
 def two_sum(nums, target)
-    val_map = {}
-    nums.each_with_index do
-        |v, i|
-        val_map[v] = i
+  num_map = Hash.new { |h, k| h[k] = [] }
+  nums.each_with_index do
+    |num, index|
+    index_array = num_map[num]
+    index_array.push(index)
+    num_map[num] = index_array
+  end
+
+  nums.each do
+    |num1|
+    num2 = target - num1
+    index_array1 = num_map[num1]
+    index_array2 = num_map[num2]
+    index_array_size = index_array2.size
+    if num1 != num2
+      if index_array_size > 0
+        return [index_array1[0], index_array2[0]]
+      end
+    else
+      if index_array_size > 1
+        return [index_array1[0], index_array2[1]]
+      end
     end
-    
-    res = []
-    a = nums.each_with_index do
-        |v, i|
-        num2 = target - v
-        puts "#{v}, #{num2}"
-        if !val_map[num2].nil?
-            i2 = val_map[num2]
-            res = [i, i2]
-            return
-        end
-    end
+  end
+  return []
 end
+
+nums = [3, 6, 4]
+target = 6
+
+p two_sum(nums, target)
