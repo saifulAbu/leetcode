b9a2d00515fe5bc83b0df5209f05ba869a85c8f7
Abu, Saif <saif.abu@cerner.com>
2020-02-20 22:56:21 -0600

    adding leetcode stuffs

--- 2_add_two_nums.rb
+++ 2_add_two_nums.rb
@@ -11,7 +11,30 @@ end
 # @param {ListNode} l2
 # @return {ListNode}
 def add_two_numbers(l1, l2)
+    h1 = l1
+    h2 = l2
+    if l1.nil? && !l2.nil?
+        return l2
+    elsif !l1.nil? && l2.nil?
+        return l1
+    elsif l1.nil? && l2.nil?
+        return nil
+    end
 
+    res_list = ListNode.new(-1)
+    tail = res_list
+    carry = 0
+    until l1.nil? or l2.nil?
+        val1 = h1.val
+        val2 = h2.val
+        sum = val1 + val2 + carry
+        resVal = sum % 10
+        carry = sum / 10
+        tail = insert_back(tail, resVal)
+        l1 = l1.next
+        l2 = l2.next
+    end
+    res_list.next
 end
 
 def insert_front(head, value)
@@ -20,21 +43,35 @@ def insert_front(head, value)
     return node
 end
 
+def insert_back(tail, value)
+    node = ListNode.new(value)
+    tail.next = node
+    return node
+end
+
 head = nil
 head = insert_front(head, 3)
 head = insert_front(head, 4)
 head = insert_front(head, 2)
-
-p head
-
 l1 = head
 
+head = nil
+head = insert_front(head, 1)
+head = insert_front(head, 0)
+head = insert_front(head, 2)
+l2 = head
+
+
+p add_two_numbers(l1, l2)
+
+
+=begin
+l1 = head
 until l1.nil?
     puts l1.val
     l1 = l1.next
 end
 
-=begin
 head = ListNode.new(3)
 prev = ListNode.new(4)
 prev.next = head
