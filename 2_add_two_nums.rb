d5e93c37ba3a24ce91ebe09debd3854d31c87d08
Abu, Saif <saif.abu@cerner.com>
2020-02-22 15:48:43 -0600

    problem 2 accepted

--- 2_add_two_nums.rb
+++ 2_add_two_nums.rb
@@ -1,4 +1,6 @@
 # Definition for singly-linked list.
+#require 'pry'
+
 class ListNode
    attr_accessor :val, :next
    def initialize(val)
@@ -20,13 +22,13 @@ def add_two_numbers(l1, l2)
     elsif l1.nil? && l2.nil?
         return nil
     end
-
     res_list = ListNode.new(-1)
     tail = res_list
     carry = 0
     until l1.nil? or l2.nil?
-        val1 = h1.val
-        val2 = h2.val
+#        binding.pry
+        val1 = l1.val
+        val2 = l2.val
         sum = val1 + val2 + carry
         resVal = sum % 10
         carry = sum / 10
@@ -34,6 +36,13 @@ def add_two_numbers(l1, l2)
         l1 = l1.next
         l2 = l2.next
     end
+
+    if l1.nil?
+        insert_rest_back(l2, tail, carry)
+    else
+        insert_rest_back(l1, tail, carry)
+    end
+
     res_list.next
 end
 
@@ -49,6 +58,21 @@ def insert_back(tail, value)
     return node
 end
 
+def insert_rest_back(list_rest, result_tail, carry)
+    until list_rest.nil?
+        val = carry + list_rest.val
+        carry = val / 10
+        val = val % 10
+        result_tail = insert_back(result_tail, val)
+        list_rest = list_rest.next
+    end
+
+    unless carry == 0
+        insert_back(result_tail, carry)
+    end
+    result_tail
+end
+
 head = nil
 head = insert_front(head, 3)
 head = insert_front(head, 4)
@@ -56,28 +80,10 @@ head = insert_front(head, 2)
 l1 = head
 
 head = nil
-head = insert_front(head, 1)
-head = insert_front(head, 0)
-head = insert_front(head, 2)
+head = insert_front(head, 7)
+head = insert_front(head, 5)
+head = insert_front(head, 7)
 l2 = head
 
 
 p add_two_numbers(l1, l2)
-
-
-=begin
-l1 = head
-until l1.nil?
-    puts l1.val
-    l1 = l1.next
-end
-
-head = ListNode.new(3)
-prev = ListNode.new(4)
-prev.next = head
-head = prev
-prev = ListNode.new(2)
-prev.next = head
-head = prev
-l1 = head
-=end
