a0e1190e8d4226277af5aaa6015e9f85da917c0d
Saiful Abu <saifulabu25@gmail.com>
2020-03-11 07:37:11 -0500

    adding 4th problem

---
+++ 4_median_of_sorted_arrays.rb
@@ -0,0 +1,23 @@
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Float}
+def find_median_sorted_arrays(nums1, nums2)
+
+end
+
+=begin
+solution idea
+=====================
+num1 = [1,6,8,9,10,11]
+num2 = [2,4,5,6,9]
+
+similar to idea of binary search
+num1 has size m
+num2 has size n
+find the element that will be on index
+  (m + n) / 2 - 1 if both the sorted arrays are combined when m+n is odd
+  (m + n) / 2 -1 and (m+n)/2 when m+n is even
+
+
+find()
+=end
