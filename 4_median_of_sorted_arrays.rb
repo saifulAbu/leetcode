9a27faf03b4f69d20ee5211ab9dd10f29e61a22b
Saiful Abu <saifulabu25@gmail.com>
2020-03-19 21:25:35 -0500

    adding median

--- 4_median_of_sorted_arrays.rb
+++ 4_median_of_sorted_arrays.rb
@@ -1,63 +1,73 @@
-# @param {Integer[]} nums1
-# @param {Integer[]} nums2
-# @return {Float}
 def find_median_sorted_arrays(nums1, nums2)
+  size1 = nums1.size
+  size2 = nums2.size
+  size_is_odd = ((size1 + size2) % 2 == 1)
 
+  val1 = 0
+  val2 = 0
+
+  median1_index = ((size1 + size2) / 2.0).ceil - 1
+  median2_index = median1_index + 1
+
+  result_index = find_index_of_rank(nums1, nums2, median1_index)
+  if result_index != -1
+    val1 = nums1[result_index]
+  else
+    result_index = find_index_of_rank(nums2, nums1, median1_index)
+    val1 = nums2[result_index]
+  end
+
+  return val1 / 1.0 if size_is_odd
+
+  result_index = find_index_of_rank(nums1, nums2, median2_index)
+  if result_index != -1
+    val2 = nums1[result_index]
+  else
+    result_index = find_index_of_rank(nums2, nums1, median2_index)
+    val2 = nums2[result_index]
+  end
+
+  return (val1 + val2) / 2.0
 end
 
-=begin
-solution idea
-=====================
-num1 = [1,6,8,9,10,11]
-num2 = [2,4,5,6,9]
+def find_index_of_rank(arr1, arr2, rank)
+  size1 = arr1.size
+  size2 = arr2.size
 
-similar to idea of binary search
-num1 has size m
-num2 has size n
-find the element that will be on index
-  (m + n) / 2 - 1 if both the sorted arrays are combined when m+n is odd
-  (m + n) / 2 -1 and (m+n)/2 when m+n is even
+  start_index = 0
+  end_index = size1 - 1
 
+  while start_index <= end_index
+    mid_index = (start_index + end_index) / 2
+    mid_rank = mid_index + find_rank(arr2, arr1[mid_index])
+    if mid_rank == rank
+      return mid_index
+    elsif mid_rank > rank
+      end_index = mid_index - 1
+    else
+      start_index = mid_index + 1
+    end
+  end
+  return -1
+end
 
-find()
-=end
+# arr cannot be null
+def find_rank(arr, val)
+  size = arr.size
+  return 0 if val < arr[0]
+  return size if val > arr[size-1]
 
-=begin
-  possibilities
-   when total size is odd
-      median is in num1
-      median is not in num1
-   when total size even
-      median1, median2 in num1
-      median1 in num1
-      median not in num1
-
-  in num1 do binary search with start_index = 0, end_index = num1.size - 1
-  find the rank of mid_index = (start_index + end_index) / 2
-    if rank == median return the median
-    if rank > median_rank search left portion of num1
-    if rank < median_rank search right portion of num1
-    stop when start_index = end_index
-    check the rank of the element where start_index = end_index, this is the final_index, final_rank
-
-    if total_number == odd
-      if final_rank == median_rank, median is found in num1
-      else search for median_rank on num2
-    if total_number == even
-      if final_rank == median_rank_1
-        rank_of_nex_elem = rank_of num1[final_index+1]
-        if rank_of_nex_elem = median_rank_2
-          median found on num1
-        else search for rank2 on num2
-      else if final_rank == median_rank2
-        rank_of_prev_elem = rank_of num1[final_index-1]
-        if rank_of_prev_elem == median_rank_1
-          median found on num1
-        else search for rank1 on num2
-      else
-        both median on num2
-        median_index_1 = search for median_rank_1 on num2
-        median_index_2 = median_index_1 + 1
-        median found on num2
-     end
-=end
+  start = 0
+  end_i = size - 1
+  mid = 0
+  while start < end_i
+    mid = (start + end_i) / 2
+    if val > arr[mid]
+      start = mid + 1
+    else
+      end_i = mid - 1
+    end
+  end
+  return (start + 1) if arr[start] < val
+  return start
+end
