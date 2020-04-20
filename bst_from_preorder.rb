3cb400bf58f4fee39ee294997eb87d2d0018470e
Saiful Abu <saifulabu25@gmail.com>
2020-04-20 07:18:50 -0500

    adding bst solution

---
+++ bst_from_preorder.rb
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+class TreeNode
+     attr_accessor :val, :left, :right
+     def initialize(val)
+         @val = val
+         @left, @right = nil, nil
+     end
+ end
+
+# @param {Integer[]} preorder
+# @return {TreeNode}
+def bst_from_preorder(preorder)
+  return nil if preorder.empty?
+  val = preorder.shift
+
+  left = []
+  right = []
+  preorder.each do
+    |elem|
+    if elem < val
+      left << elem
+    else
+      right << elem
+    end
+  end
+
+  node = TreeNode.new(val)
+  node.left = bst_from_preorder(left)
+  node.right = bst_from_preorder(right)
+  return node
+end
+
+p bst_from_preorder([])
+p bst_from_preorder([8,5,1,7,10,12])
