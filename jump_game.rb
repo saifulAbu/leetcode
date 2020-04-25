0c4a4b09f5b4efe17143dd7d89b0011dca236411
Saiful Abu <saifulabu25@gmail.com>
2020-04-25 13:13:45 -0500

    adding greedy solution O(n)

--- jump_game.rb
+++ jump_game.rb
@@ -2,19 +2,12 @@
 # @return {Boolean}
 def can_jump(nums)
   size = nums.size
-  possible = Array.new(size) {false}
-  possible[size-1] = true
-
+  last_known_good = size - 1
   (size-2).downto(0) do
     |i|
-    max_jump = nums[i]
-    max_jump.downto(1) do
-      |cur_jump|
-      if (i + cur_jump >= (size - 1)) or (possible[i + cur_jump])
-        possible[i] = true
-        break
-      end
-    end
+    last_known_good = i if i + nums[i] >= last_known_good
   end
-  possible[0]
+
+  last_known_good == 0
 end
+
