913b06df02839c8ca8f14f8cae46c66a6123015c
Saiful Abu <saifulabu25@gmail.com>
2020-04-24 22:08:26 -0500

    adding lru cache

--- bitwise_and_of_range_numbers.rb
+++ bitwise_and_of_range_numbers.rb
@@ -5,8 +5,13 @@ def range_bitwise_and(m, n)
   return m if m == n
   result = 0;
   cur_bit = 1;
+  mask = 2 ** (m.bit_length + 1) - 1
+
   while cur_bit <= m
-    if m + cur_bit > n
+    mask = mask ^ cur_bit
+    #puts mask
+    minimum_num_bit_flip = (m + cur_bit) & mask
+    if minimum_num_bit_flip > n
       result = result | cur_bit
     end
     cur_bit = cur_bit * 2
@@ -14,4 +19,4 @@ def range_bitwise_and(m, n)
   result
 end
 
-p range_bitwise_and 1, 1
+p range_bitwise_and 3, 4
