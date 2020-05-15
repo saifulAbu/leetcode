59c90b694be530c6521868992f06f8cc9dee9393
Saiful Abu <saifulabu25@gmail.com>
2020-05-14 21:30:44 -0500

    adding remove k digits solution

---
+++ 402_remove_k_digits.rb
@@ -0,0 +1,34 @@
+# @param {String} num
+# @param {Integer} k
+# @return {String}
+def remove_kdigits(num, k)
+  q = num.chars
+  s = ["0"]
+  until q.empty? or k == 0
+    if s.last > q.first
+      s.pop
+      k -= 1
+    else
+      s << q.shift
+    end
+  end
+
+  until k == 0
+    s.pop
+    k -= 1
+  end
+
+  s.delete_at(0)
+  res = s + q
+  res.join
+
+  while res.first == "0"
+    res.shift
+  end
+
+  res = "0" if res = ""
+
+  res
+end
+
+p remove_kdigits("112", 2)
