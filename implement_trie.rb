69aa72ca4c9182b836c1fb02d78b42120befa1e8
Saiful Abu <saifulabu25@gmail.com>
2020-05-19 22:07:53 -0500

    add trie solution

--- implement_trie.rb
+++ implement_trie.rb
@@ -2,7 +2,7 @@ class Trie
   class TrieNode
     def initialize()
       @children = {}
-      @complete = {}
+      @complete = Hash.new() {false}
     end
 
     def get_children(ch)
@@ -34,26 +34,41 @@ class Trie
       if child.nil?
         child = TrieNode.new
         node.set_children(ch, child)
-        node = child
       end
+      node = child
     end
     node.set_complete(word[-1])
+  end
 
-    p @root
+  def get_last_node(word)
+    node = @root
+    word.each_char do
+      |ch|
+      node = node.get_children(ch)
+      return nil if node.nil?
+    end
+    return node
   end
 
   def search(word)
-
+    last_node = get_last_node(word)
+    return false if last_node.nil?
+    return true if last_node.is_complete?(word[-1])
+    return false
   end
 
   def starts_with(prefix)
-    #some dummy commit
+    last_node = get_last_node(prefix)
+    return false if last_node.nil?
+    return true
   end
 end
 
 # Your Trie object will be instantiated and called as such:
  word = "word"
  obj = Trie.new()
- obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.starts_with(prefix)
+ #obj.insert("saif")
+ #obj.insert("sai")
+ #obj.insert("sa")
+p param_2 = obj.search("s")
+#p param_3 = obj.starts_with("z")
