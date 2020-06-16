860c49abde4c4f3b6cd692aa710f003c2681ad59
Saiful Abu <saifulabu25@gmail.com>
2020-06-16 06:23:07 -0500

    adding odd even linked list solution

--- odd_even_linked_list.rb
+++ odd_even_linked_list.rb
@@ -10,7 +10,32 @@
 # @param {ListNode} head
 # @return {ListNode}
 def odd_even_list(head)
+  even_head = ListNode.new("dummy")
+  even_tail = even_head
+  odd_head = ListNode.new("dummy")
+  odd_tail = odd_head
+  cur_node_number = 1
+  cur_head = head
 
+  until cur_head.nil?
+    cur_node = cur_head
+    cur_head = cur_head.next
+    cur_node.next = nil
+
+    if cur_node_number.even?
+      even_tail.next = cur_node
+      even_tail = cur_node
+    else
+      odd_tail.next = cur_node
+      odd_tail = cur_node
+    end
+
+    cur_node_number += 1
+  end
+
+  result = odd_head.next
+  odd_tail.next = even_head.next
+  result
 end
 
 head = ListNode.new(6)
@@ -20,5 +45,5 @@ head = ListNode.new(6)
   head = ListNode.new(i, head)
 end
 
-p head
-#res = odd_even_list
+res = odd_even_list head
+p res
