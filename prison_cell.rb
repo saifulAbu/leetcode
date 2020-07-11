8390c040688ff1c38de89cd1201679c42189e972
Saiful Abu <saifulabu25@gmail.com>
2020-07-11 16:49:07 -0500

    adding today's contributino

--- prison_cell.rb
+++ prison_cell.rb
@@ -11,10 +11,12 @@ def prison_after_n_days(cells, n)
 
   period = 1
   cur_day = cells
- # p cur_day
+  p cur_day
   while true
     cur_day = next_day(cur_day)
+    p cur_day
     if set.include? cur_day
+      puts "cycle"
       break
     end
     #p cur_day
@@ -38,11 +40,21 @@ def next_day(prev_day)
   res
 end
 
-start = [1,0,0,1,0,0,1,0]
-p prison_after_n_days(start, 15)
+start = [0,0,0,0,0,0,0,0]
+prison_after_n_days(start, 15)
 
 
 =begin
+note for sat july 11
+make a reverse map of cell => day
+figure out which day the repetition began
+
+if repetition
+ period_len = cur_day - map[cur_day]
+end
+
+final_index = (N - day_rep_began) % period_len + day_rep_began
+
 size of the cells is fixed, r rows. n can be between 1 to 10 ^ 9
 so content of cells will repeat.
 
