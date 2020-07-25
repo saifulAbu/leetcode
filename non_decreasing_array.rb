eb8c42e11ec7784459244e8743ada5e05988f60b
Saiful Abu <saifulabu25@gmail.com>
2020-07-24 23:21:04 -0500

    adding initial wrong solution

---
+++ non_decreasing_array.rb
@@ -0,0 +1,23 @@
+# @param {Integer[]} nums
+# @return {Boolean}
+def check_possibility(nums)
+  n = nums.size
+  return true if n == 1
+  modification_done = false
+  for i in (1..(n-1))
+    if nums[i-1] > nums[i]
+      if modification_done
+        return false
+      else
+        modification_done = true
+      end
+    end
+  end
+  return true
+end
+
+nums = [3, 4, 2, 3]
+p check_possibility(nums)
+
+nums = [4, 2, 1]
+p check_possibility(nums)
