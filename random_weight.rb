86570f30d1850d661be9d1f8fa744f1e4d569f22
Saiful Abu <saifulabu25@gmail.com>
2020-09-26 13:42:21 -0500

    before translation

--- random_weight.rb
+++ random_weight.rb
@@ -3,36 +3,49 @@ class Solution
     :type w: Integer[]
 =end
   def initialize(w)
-    @prob = Array.new(w.size)
+    @range_bucket = Array.new(w.size)
     total_sum = 0.0
 
     for weight in w
       total_sum += weight
     end
 
+    lower_bound = 0.0
+    upper_bound = 0.0
     for i in (0..(w.size-1)) do
-      @prob[i] = (w[i] / total_sum)
+      @range_bucket[i] = [lower_bound, upper_bound]
+      lower_bound = upper_bound
     end
+    p @range_bucket
   end
 =begin
     :rtype: Integer
 =end
   def pick_index()
     rand_num = rand
-    cum_prob = 0
-    i = 0
-    while(i < @prob.size)
-      cum_prob += @prob[i]
-      break if cum_prob > rand_num
-      i += 1
+    index = 0
+    s = 0
+    e = @range_bucket.size - 1
+    while(s <= e)
+      m = s + (e - s) / 2
+     # puts "s: #{s}, m:#{m}, e:#{e}"
+      cur_bound = @range_bucket[m]
+      if(rand_num >= cur_bound[0] and rand_num <= cur_bound[1])
+        index = m
+        break
+      elseif (rand_num < cur_bound[0])
+        e = m - 1
+      else
+        s = m + 1
+      end
     end
-    puts i
-    return i
+    puts index
+    return index
   end
 end
 
 # Your Solution object will be instantiated and called as such:
-w = [4, 1]
+w = [1, 2, 3]
 obj = Solution.new(w)
 param_1 = obj.pick_index()
 param_1 = obj.pick_index()
