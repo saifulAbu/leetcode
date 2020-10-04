52a012bafa56de750802e9371aecb0e55c81ef18
Saiful Abu <saifulabu25@gmail.com>
2020-10-03 20:47:47 -0500

    adding coin change 2 solution

--- coin_exchange_2.rb
+++ coin_exchange_2.rb
@@ -2,5 +2,32 @@
 # @param {Integer[]} coins
 # @return {Integer}
 def change(amount, coins)
+  return 1 if amount == 0
 
+  dp = Array.new(coins.size + 1) { Array.new(amount + 1)}
+
+  for i in (0..(coins.size)) do
+    dp[i][0] = 1
+  end
+
+  for j in (1..amount) do
+    dp[0][j] = 0
+  end
+
+  p dp
+
+  for i in (1..coins.size) do
+    for j in (1..amount) do
+      dp[i][j] = 0
+      dp[i][j] = dp[i][j-coins[i-1]] if j-coins[i-1] >= 0
+      dp[i][j] += dp[i-1][j]
+    end
+  end
+
+  dp[i][j]
 end
+
+amount = 10
+coins = []
+
+change(amount, coins)
