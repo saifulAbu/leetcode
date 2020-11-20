88ed62fa2284ec24fa63302984eab00440cb84e9
Saiful Abu <saifulabu25@gmail.com>
2020-11-19 21:26:20 -0600

    unsolved mystery... good enough

--- longest_duplicate_substring.rb
+++ longest_duplicate_substring.rb
@@ -1,18 +1,54 @@
+require 'set'
+#require 'pry'
+#require 'pry-nav'
+
 # @param {String} s
 # @return {String}
 def longest_dup_substring(s)
-  h = s.size - 1
-  l = 0
-  max = 0
+  h = s.size
+  l = 1
+  max_len = 0
+  max_str = ""
+  #binding.pry
   while l <= h do
     m = (h + l) / 2
-    map = {}
+    set = Set.new
     cur_hash = 0
 
-    for(i in (0..m-1))
-      cur_hash = 26 * cur_hash + s[i]
+    for i in (0..m-1)
+      cur_hash = 26 * cur_hash + int_val(s[i])
     end
-    map
+    #puts "cur_hash: #{cur_hash}"
+    set.add(cur_hash)
+    cur_len = 0
+    cur_str = ""
+    for i in (m..(s.size-1))
+      cur_hash = (cur_hash - (26 ** (m-1)) * int_val(s[i-m])) * 26 + int_val(s[i])
+      #puts "cur_hash: #{cur_hash}"
+      if(set.include?(cur_hash))
+        cur_len = m
+        cur_str = s[(i-m+1..i)]
+        break
+      else
+        set.add(cur_hash)
+      end
+    end
+
+    if(cur_len > max_len)
+      max_len = cur_len
+      max_str = cur_str
+      l = m + 1
+    else
+      h = m - 1
+    end
   end
+
+  max_str
 end
 
+def int_val(ch)
+  return ch.ord - 'a'.ord + 1
+end
+
+
+p longest_dup_substring "axydxya"
