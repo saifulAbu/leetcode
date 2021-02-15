4eb99d8c0ed2f524e286bc72b80823e777fb1e5c
Saiful Abu <saifulabu25@gmail.com>
2021-02-14 21:00:10 -0600

    adding cycle check

---
+++ amazon/src/main/java/adhoc/linked_list/CycleCheckList_141.java
@@ -0,0 +1,36 @@
+package adhoc.linked_list;
+
+public class CycleCheckList_141 {
+  public boolean hasCycle(ListNode head) {
+    ListNode slowPointer = head;
+    ListNode fastPointer = head;
+
+    while(slowPointer != null && fastPointer != null) {
+      slowPointer = slowPointer.next;
+      fastPointer = fastPointer.next;
+      if(fastPointer != null) {
+        fastPointer = fastPointer.next;
+      }
+      if(slowPointer != null && slowPointer == fastPointer) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static void main(String args[]) {
+    ListNode l0 = new ListNode(0);
+    ListNode l1 = new ListNode(1);
+    ListNode l2 = new ListNode(2);
+    ListNode l3 = new ListNode(3);
+
+    l0.next = l1;
+    l1.next = l2;
+    l2.next = l3;
+    l3.next = l1;
+
+    CycleCheckList_141 c = new CycleCheckList_141();
+    System.out.println(c.hasCycle(l0));
+
+  }
+}
---
+++ amazon/src/main/java/adhoc/linked_list/ListNode.java
@@ -0,0 +1,10 @@
+package adhoc.linked_list;
+
+class ListNode {
+  int val;
+  ListNode next;
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
