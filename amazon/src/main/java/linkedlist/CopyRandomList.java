f7ce76d8654d5a68917af5e9dc0fad6b821b271a
Saiful Abu <saifulabu25@gmail.com>
2021-02-20 19:42:24 -0600

    adding random list

--- amazon/src/main/java/linkedlist/CopyRandomList.java
+++ amazon/src/main/java/linkedlist/CopyRandomList.java
@@ -19,6 +19,7 @@ public class CopyRandomList {
   }
 
   public Node copyRandomList(Node head) {
+    if(head == null) return null;
     Node curHead = head;
 
     while(curHead != null) {
@@ -32,16 +33,27 @@ public class CopyRandomList {
     Node res = head.next;
     curHead = head;
 
+    //fix random pointer
     while(curHead != null) {
-      Node nextHead = curHead.next.next;
       Node copy = curHead.next;
-      Node nextAfterCopy = copy.next;
-      if(copy.next != null) {
-        copy.next = copy.next.next;
-      }
+      Node nextHead = copy.next;
+
       if(curHead.random != null) {
         copy.random = curHead.random.next;
       }
+      curHead = nextHead;
+    }
+
+    curHead = head;
+    //fix next pointer
+    while(curHead != null) {
+      Node copy = curHead.next;
+      Node nextHead = copy.next;
+
+      if(nextHead != null) {
+        copy.next = nextHead.next;
+      }
+
       curHead.next = nextHead;
       curHead = nextHead;
     }
@@ -49,26 +61,47 @@ public class CopyRandomList {
   }
 
   public static void main(String args[]) {
-    Node n7 = new Node(7);
-    Node n13 = new Node(13);
-    Node n11 = new Node(11);
-    Node n10 = new Node(10);
+//    Node n7 = new Node(7);
+//    Node n13 = new Node(13);
+//    Node n11 = new Node(11);
+//    Node n10 = new Node(10);
+//    Node n1 = new Node(1);
+//
+//    n7.next = n13;
+//    n13.next = n11;
+//    n11.next = n10;
+//    n10.next = n1;
+//
+//    n7.random = null;
+//    n13.random = n7;
+//    n11.random = n1;
+//    n10.random = n11;
+//    n1.random = n7;
+    Node n0 = new Node(0);
     Node n1 = new Node(1);
+    Node n2 = new Node(2);
+    Node n3 = new Node(3);
 
-    n7.next = n13;
-    n13.next = n11;
-    n11.next = n10;
-    n10.next = n1;
+    n1.next = n2;
+    n2.next = n3;
 
-    n7.random = null;
-    n13.random = n7;
-    n11.random = n1;
-    n10.random = n11;
-    n1.random = n7;
-
+    n2.random = n1;
+    n3.random = n2;
     CopyRandomList c = new CopyRandomList();
-    Node r = c.copyRandomList(n7);
+    Node r = c.copyRandomList(n1);
+
+
+     c.printList(n1);
+     System.out.println();
+     c.printList(r);
     return;
+  }
+
+  void printList(Node head) {
+    while(head != null) {
+      head.print();
+      head = head.next;
+    }
   }
 
 }
