946a2d7b623ecc0939de041d4ed224c0ece64aaa
Saiful Abu <saifulabu25@gmail.com>
2021-03-28 12:50:42 -0500

    adding solutiong

--- amazon/src/main/java/array_and_string/SplitStringBalanced_1221.java
+++ amazon/src/main/java/array_and_string/SplitStringBalanced_1221.java
@@ -2,6 +2,21 @@ package array_and_string;
 
 public class SplitStringBalanced_1221 {
   public int balancedStringSplit(String s) {
-    return 0;
+    char [] charArr = s.toCharArray();
+    int count = 0;
+    int balance = 0;
+    for(int i = 0; i < charArr.length; i++) {
+      char ch = charArr[i];
+      if(ch == 'L'){
+        balance++;
+      } else {
+        balance--;
+      }
+      if(balance == 0) {
+        count++;
+      }
+
+    }
+    return count;
   }
 }
