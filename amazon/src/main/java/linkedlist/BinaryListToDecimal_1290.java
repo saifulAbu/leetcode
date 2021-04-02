8d17f1127c49aaeec2407a9983e2581ee95cbda7
Saiful Abu <saifulabu25@gmail.com>
2021-04-01 21:11:05 -0500

    adding more stuffs

---
+++ amazon/src/main/java/linkedlist/BinaryListToDecimal_1290.java
@@ -0,0 +1,12 @@
+package linkedlist;
+
+public class BinaryListToDecimal_1290 {
+  public int getDecimalValue(ListNode head) {
+    int num = 0;
+    while(head != null) {
+      num = num * 2 + head.val;
+      head = head.next;
+    }
+    return num;
+  }
+}
