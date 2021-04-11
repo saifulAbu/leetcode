e62876339ee5c55c28aa2cc124c758c2606442ac
Saiful Abu <saifulabu25@gmail.com>
2021-04-10 22:08:25 -0500

    adding today's

--- amazon/src/main/java/array_and_string/ReverseInteger_7.java
+++ amazon/src/main/java/array_and_string/ReverseInteger_7.java
@@ -1,5 +1,22 @@
+package array_and_string;
+
 public class ReverseInteger_7 {
     public int reverse(int x) {
-        //some randomnneses
+        int output = 0;
+
+        while(x != 0) {
+            int curDigit = x % 10;
+            x = x / 10;
+
+            if(output > Integer.MAX_VALUE / 10 || (output == Integer.MAX_VALUE / 10 && curDigit > Integer.MAX_VALUE % 10)) {
+                return 0;
+            } else if(output < Integer.MIN_VALUE / 10 || (output == Integer.MIN_VALUE / 10 && curDigit > Integer.MAX_VALUE % 10)) {
+                return 0;
+            } else {
+                output = output * 10 + curDigit;
+            }
+        }
+
+        return output;
     }
 }
