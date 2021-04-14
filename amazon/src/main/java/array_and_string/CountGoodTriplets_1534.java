a52041119e825b2493c49fcd5dce1c2a193967a9
Abu, Saif <saif.abu@cerner.com>
2021-04-13 20:09:23 -0500

    adding good tripled

--- amazon/src/main/java/array_and_string/CountGoodTriplets_1534.java
+++ amazon/src/main/java/array_and_string/CountGoodTriplets_1534.java
@@ -2,6 +2,21 @@ package array_and_string;
 
 public class CountGoodTriplets_1534 {
   public int countGoodTriplets(int[] arr, int a, int b, int c) {
+    int goodTriplets = 0;
+    int n = arr.length;
     // do brute force search
+    for(int i = 0; i < n - 2; i++) {
+      for (int j = i + 1; j < n - 1; j++) {
+        for (int k = j + 1; k < n ; k++) {
+          if( Math.abs(arr[i] - arr[j]) <= a &&
+              Math.abs(arr[j] - arr[k]) <= b &&
+              Math.abs(arr[k] - arr[i]) <= c
+          ) {
+            goodTriplets++;
+          }
+        }
+      }
+    }
+    return goodTriplets;
   }
 }
