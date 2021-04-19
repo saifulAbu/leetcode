1248b2df2c63c8164520a1b2ba07c085505df41d
Saiful Abu <saifulabu25@gmail.com>
2021-04-18 23:37:22 -0500

    adding a bit more stuffs

--- amazon/src/main/java/array_and_string/LongestCommonPrefix_14.java
+++ amazon/src/main/java/array_and_string/LongestCommonPrefix_14.java
@@ -4,6 +4,12 @@ public class LongestCommonPrefix_14 {
   public String longestCommonPrefix(String[] strs) {
     //look at first character of all the strings
     // keey going until you find a mismatch
+    int len = 0;
+    int minLen = Integer.max();
+    for(int i = 0; i < strs.length; i++) {
+      minLen = Math.min(strs[i].length(), minLen);
+    }
+    return strs[0].substring(0, len+1);
   }
   //no idea yet
 }
