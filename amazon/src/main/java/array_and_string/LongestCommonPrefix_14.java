a85d0909e7109cfe4b63ae523f741b922dd97b83
Saiful Abu <saifulabu25@gmail.com>
2021-04-19 21:32:40 -0500

    adding a solution keep moving

--- amazon/src/main/java/array_and_string/LongestCommonPrefix_14.java
+++ amazon/src/main/java/array_and_string/LongestCommonPrefix_14.java
@@ -4,10 +4,28 @@ public class LongestCommonPrefix_14 {
   public String longestCommonPrefix(String[] strs) {
     //look at first character of all the strings
     // keey going until you find a mismatch
-    int len = 0;
-    int minLen = Integer.max();
+    if(strs.length == 0) {
+      return "";
+    }
+
+    int minLen = Integer.MAX_VALUE;
     for(int i = 0; i < strs.length; i++) {
       minLen = Math.min(strs[i].length(), minLen);
+    }
+
+    int len = 0;
+    for(; len < minLen; len++) {
+      char cur = strs[0].charAt(len);
+      boolean mismatch = false;
+      for(int i = 0; i < strs.length; i++) {
+        String str = strs[i];
+        if(str.charAt(len) != cur) {
+          mismatch = true;
+        }
+      }
+      if(mismatch) {
+        break;
+      }
     }
     return strs[0].substring(0, len+1);
   }
