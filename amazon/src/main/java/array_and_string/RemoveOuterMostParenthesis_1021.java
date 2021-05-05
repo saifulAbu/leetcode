c9e21fb650fd51c852d48a59a1be59531c2d8b98
Abu, Saif <saif.abu@cerner.com>
2021-05-05 07:55:00 -0500

    adding today's

--- amazon/src/main/java/array_and_string/RemoveOuterMostParenthesis_1021.java
+++ amazon/src/main/java/array_and_string/RemoveOuterMostParenthesis_1021.java
@@ -2,6 +2,21 @@ package array_and_string;
 
 public class RemoveOuterMostParenthesis_1021 {
   public String removeOuterParentheses(String S) {
-
+    StringBuilder sb = new StringBuilder();
+    int count = 0;
+    for(char ch : S.toCharArray()) {
+      if (ch == '(') {
+        count++;
+        if (count != 1) {
+          sb.append(ch);
+        }
+      } else {
+        count--;
+        if (count != 0) {
+          sb.append(ch);
+        }
+      }
+    }
+    return sb.toString();
   }
 }
