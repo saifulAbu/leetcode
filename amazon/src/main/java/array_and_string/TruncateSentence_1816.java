4905f9529680d7627de9d6977a636acff19d33c4
Abu, Saif <saif.abu@cerner.com>
2021-05-06 22:05:34 -0500

    adding dummy today's

---
+++ amazon/src/main/java/array_and_string/TruncateSentence_1816.java
@@ -0,0 +1,8 @@
+package array_and_string;
+
+public class TruncateSentence_1816 {
+  public String truncateSentence(String s, int k) {
+    //find i of  k th space
+    //return substring of (0, i)
+  }
+}
