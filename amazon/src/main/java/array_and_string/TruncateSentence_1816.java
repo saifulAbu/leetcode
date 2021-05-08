f04f105138683d1ea36445a09bf5517d6bcf383a
Abu, Saif <saif.abu@cerner.com>
2021-05-07 23:03:59 -0500

    today's

--- amazon/src/main/java/array_and_string/TruncateSentence_1816.java
+++ amazon/src/main/java/array_and_string/TruncateSentence_1816.java
@@ -2,7 +2,18 @@ package array_and_string;
 
 public class TruncateSentence_1816 {
   public String truncateSentence(String s, int k) {
+    int spaceCount = 0;
     //find i of  k th space
+    int i = 0;
+    for(; i < s.length(); i++) {
+      if (s.charAt(i) == ' ') {
+        spaceCount++;
+      }
+      if (spaceCount == k) {
+        break;;
+      }
+    }
+    return s.substring(0, i);
     //return substring of (0, i)
   }
 }
