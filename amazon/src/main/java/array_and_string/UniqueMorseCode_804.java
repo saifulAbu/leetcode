fe5ce572dc3af2ca4d020f4dd702f272c86b19e8
Abu, Saif <saif.abu@cerner.com>
2021-05-11 20:54:38 -0500

    adding today's

--- amazon/src/main/java/array_and_string/UniqueMorseCode_804.java
+++ amazon/src/main/java/array_and_string/UniqueMorseCode_804.java
@@ -1,8 +1,26 @@
 package array_and_string;
 
-public class UniqueMorseCode_804 {
-  public int uniqueMorseRepresentations(String[] words) {
+import java.util.HashSet;
+import java.util.Set;
 
+public class UniqueMorseCode_804 {
+  private String [] alphabet = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+  public int uniqueMorseRepresentations(String[] words) {
+    // form word and put in a set
+    Set<String> morseSet = new HashSet<>();
+    for(String word : words) {
+      morseSet.add(formWord(word));
+    }
+    return morseSet.size();
+  }
+
+  private String formWord(String word) {
+    String morse = "";
+    for (char c : word.toCharArray()) {
+      int i = c - 'a';
+      morse = morse + alphabet[i];
+    }
+    return morse;
   }
 }
 
