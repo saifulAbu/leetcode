e72fcc1b6873b8767db7e82685aeafa863a80055
Saiful Abu <saifulabu25@gmail.com>
2021-05-18 20:26:51 -0500

    solution

--- amazon/src/main/java/array_and_string/MaxPopulation_1854.java
+++ amazon/src/main/java/array_and_string/MaxPopulation_1854.java
@@ -2,9 +2,22 @@ package array_and_string;
 
 public class MaxPopulation_1854 {
   public int maximumPopulation(int[][] logs) {
-
+    int max = 0;
+    int maxY = 0;
+    for(int year = 1950; year <= 2050; year++) {
+      int cur = 0;
+      for(int[] y : logs) {
+        int birth = y [0];
+        int death = y [1];
+        if(birth <= year && year < death ) {
+          cur++;
+        }
+      }
+      if(cur > max) {
+        max = cur;
+        maxY = year;
+      }
+    }
+    return maxY;
   }
 }
-// still no idea
-// no idea yet
-// no idea yet
