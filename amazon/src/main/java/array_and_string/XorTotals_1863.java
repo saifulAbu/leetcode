ae92439f6fdb01e7f3e5786d5b577cdadb70f32e
Abu, Saif <saif.abu@cerner.com>
2021-05-19 22:59:12 -0500

    adding idea

---
+++ amazon/src/main/java/array_and_string/XorTotals_1863.java
@@ -0,0 +1,9 @@
+package array_and_string;
+
+public class XorTotals_1863 {
+  public int subsetXORSum(int[] nums) {
+    // for n, loop through all 2 ^ n numbers
+    // for each i, figure out the elements of the array
+    // xor, add them up
+  }
+}
