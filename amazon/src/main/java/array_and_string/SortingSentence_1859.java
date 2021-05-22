1bf166b940933cb8e22f0429f490b394e56179d4
Saiful Abu <saifulabu25@gmail.com>
2021-05-22 04:18:58 -0500

    adding today's

--- amazon/src/main/java/array_and_string/SortingSentence_1859.java
+++ amazon/src/main/java/array_and_string/SortingSentence_1859.java
@@ -1,7 +1,30 @@
 package array_and_string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SortingSentence_1859 {
   public String sortSentence(String s) {
+    String [] sarr = s.split(" ");
+    Map<Integer, String> m = new HashMap<>();
 
+    for(String str : sarr) {
+      int index = str.charAt(str.length()-1) - '1';
+      m.put(index, str.substring(0, str.length() - 1));
+    }
+
+    String res = m.get(0);
+    for(int i = 1; i < sarr.length; i++) {
+      res = res + " " + m.get(i);
+    }
+
+    System.out.println(res);
+    return res;
+  }
+
+  public static void main(String args[]) {
+    SortingSentence_1859 s = new SortingSentence_1859();
+    String str = "is2 sentence4 This1 a3";
+    s.sortSentence(str);
   }
 }
