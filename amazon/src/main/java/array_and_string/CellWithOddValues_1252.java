4125ac28324233d44194afaf39571a4319b687f0
Abu, Saif <saif.abu@cerner.com>
2021-05-27 22:23:04 -0500

    adding today's

--- amazon/src/main/java/array_and_string/CellWithOddValues_1252.java
+++ amazon/src/main/java/array_and_string/CellWithOddValues_1252.java
@@ -2,7 +2,30 @@ package array_and_string;
 
 public class CellWithOddValues_1252 {
   public int oddCells(int m, int n, int[][] indices) {
-    return 0;
+    int [][] base = new int[m][n];
+
+    for(int [] index : indices) {
+      int curR = index[0];
+      int curC = index[1];
+      //fill up row
+      for(int c = 0; c < n; c++) {
+        base[curR][c]++;
+      }
+      //fill up col
+      for(int r = 0; r < m; r++) {
+        base[r][curC]++;
+      }
+    }
+
+    int count = 0;
+    for (int r = 0; r < m; r++) {
+      for (int c = 0; c < n; c++) {
+        if (base[r][c] % 2 != 0) {
+          count++;
+        }
+      }
+    }
+    return count;
   }
 }
 
