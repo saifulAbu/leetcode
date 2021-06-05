259b81bd3164967a54a771f3658716bdb84ad312
Abu, Saif <saif.abu@cerner.com>
2021-06-04 22:16:37 -0500

    adding solutiong

--- amazon/src/main/java/array_and_string/CheckSummation_1880.java
+++ amazon/src/main/java/array_and_string/CheckSummation_1880.java
@@ -2,6 +2,23 @@ package array_and_string;
 
 public class CheckSummation_1880 {
   public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+    int first = getNumber(firstWord);
+    int second = getNumber(secondWord);
+    int target = getNumber(targetWord);
 
+    return (first + second) == target;
+  }
+
+  private int getNumber(String str) {
+    int num = 0;
+    for(char ch : str.toCharArray()) {
+      int intVal = getIntVal(ch);
+      num = num * 10 + intVal;
+    }
+    return num;
+  }
+
+  private int getIntVal(char ch) {
+    return (int) (ch - 'a');
   }
 }
