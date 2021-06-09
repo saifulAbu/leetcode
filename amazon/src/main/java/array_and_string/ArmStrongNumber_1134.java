56f07813d3760e47d8d7e38f762052c353d05b09
Abu, Saif <saif.abu@cerner.com>
2021-06-09 15:31:08 -0500

    adding armstrong

---
+++ amazon/src/main/java/array_and_string/ArmStrongNumber_1134.java
@@ -0,0 +1,20 @@
+package array_and_string;
+
+public class ArmStrongNumber_1134 {
+  public boolean isArmstrong(int n) {
+    int num = n;
+    int sum = 0;
+    int numDigit = (int) (Math.log10(n) + 1);
+
+    while (num > 0) {
+      int curdigit = num % 10;
+      sum += Math.pow(curdigit, numDigit);
+      num /= 10;
+    }
+
+    if (sum == n) {
+      return true;
+    }
+    return false;
+  }
+}
