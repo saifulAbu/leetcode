74b5047b717c9df6dbc04f322c5cea05c8ea7c94
Saiful Abu <saifulabu25@gmail.com>
2021-06-14 19:24:34 -0500

    one

---
+++ amazon/src/main/java/array_and_string/NumberOfStudentHomework_1450.java
@@ -0,0 +1,15 @@
+package array_and_string;
+
+public class NumberOfStudentHomework_1450 {
+  public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+    int res = 0;
+
+    for(int i = 0; i < startTime.length; i++) {
+      if(startTime[i] <= queryTime && queryTime <= endTime[i]) {
+        res++;
+      }
+    }
+
+    return res;
+  }
+}
