65e48027f33a93c6d9fd549552e452e96d032859
Saiful Abu <saifulabu25@gmail.com>
2021-06-24 08:30:26 -0500

    today

--- amazon/src/main/java/array_and_string/LengthOfLastWord_58.java
+++ amazon/src/main/java/array_and_string/LengthOfLastWord_58.java
@@ -2,6 +2,10 @@ package array_and_string;
 
 public class LengthOfLastWord_58 {
   public int lengthOfLastWord(String s) {
+    String[] splt = s.split(" ");
+    if(splt.length > 0) {
+      return splt[splt.length - 1].length();
+    }
     return 0;
   }
 }
