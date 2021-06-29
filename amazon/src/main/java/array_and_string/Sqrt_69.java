86b971db0a16d47c2ed73db6630946fbf1131175
Abu, Saif <saif.abu@cerner.com>
2021-06-28 20:42:15 -0500

    binary search

--- amazon/src/main/java/array_and_string/Sqrt_69.java
+++ amazon/src/main/java/array_and_string/Sqrt_69.java
@@ -2,6 +2,29 @@ package array_and_string;
 
 public class Sqrt_69 {
   public int mySqrt(int x) {
+    if(x <= 1) {
+      return x;
+    }
+    double res = 0;
+    double low = 0;
+    double high = x;
+    while(low <= high) {
+      double mid = (low + (high - low) / 2);
+      double mid_sqr = mid * mid;
+      if(mid_sqr > x) {
+        high = mid - 1;
+      } else if (mid_sqr < x) {
+        res = mid;
+        low = mid + 1;
+      } else {
+        res = mid;
+        break;
+      }
+    }
+    return (int)res;
+  }
+
+  public int mySqrt1(int x) {
     int res = 0;
     int low = 0;
     int high = x;
@@ -19,5 +42,10 @@ public class Sqrt_69 {
       }
     }
     return res;
+  }
+
+  public static void main(String args[]) {
+    Sqrt_69 s = new Sqrt_69();
+    System.out.println(s.mySqrt(5));
   }
 }
