b52339453bfb0d3ef666d81e3178c6da783d9c9c
Saiful Abu <saifulabu25@gmail.com>
2021-07-01 22:09:35 -0500

    adding remove duplicate solution

--- amazon/src/main/java/linkedlist/RemoveDuplicates_83.java
+++ amazon/src/main/java/linkedlist/RemoveDuplicates_83.java
@@ -2,6 +2,15 @@ package linkedlist;
 
 public class RemoveDuplicates_83 {
   public ListNode deleteDuplicates(ListNode head) {
-
+    ListNode front = head;
+    while(front != null) {
+      ListNode back = front.next;
+      while(back != null && back.val == front.val) {
+        back = back.next;
+      }
+      front.next = back;
+      front = front.next;
+    }
+    return head;
   }
 }
