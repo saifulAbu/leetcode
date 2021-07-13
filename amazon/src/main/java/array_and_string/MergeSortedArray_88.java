37b752737c8cd6df38b63226e8bfeef325b41f86
Abu, Saif <saif.abu@cerner.com>
2021-07-13 00:07:14 -0500

    one more

--- amazon/src/main/java/array_and_string/MergeSortedArray_88.java
+++ amazon/src/main/java/array_and_string/MergeSortedArray_88.java
@@ -1,11 +1,21 @@
 package array_and_string;
 
 public class MergeSortedArray_88 {
-  public void merge(int[] nums1, int m, int[] nums2, int n) {
+  public void merge(int[] a0, int m, int[] a1, int n) {
+    int i0 = m - 1;
+    int i1 = n - 1;
+    int i = a0.length - 1;
 
+    while (i0 >= 0 && i1 >= 0) {
+      a0[i--] = (a0[i0] > a1[i1] ? a0[i0--] : a1[i1--]);
+    }
+
+    while (i1 >= 0) {
+      a0[i--] = a1[i1--];
+    }
   }
 }
-
+// false
 //dummy commit for today
 // more dummy commit
 // more dummy commit
