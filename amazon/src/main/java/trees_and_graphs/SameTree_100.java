5b57752fd739b452aa2a40da2f652c7e2f5a6914
Saiful Abu <saifulabu25@gmail.com>
2021-07-16 23:37:39 -0500

    add more

---
+++ amazon/src/main/java/trees_and_graphs/SameTree_100.java
@@ -0,0 +1,20 @@
+package trees_and_graphs;
+
+public class SameTree_100 {
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+      if(p == null && q == null) {
+        return true;
+      }
+
+      if(p == null || q == null) {
+        return false;
+      }
+
+      if(p.val != q.val || !(isSameTree(p.left, q.left))) {
+        return false;
+      }
+
+      return isSameTree(p.right, q.right);
+  }
+}
