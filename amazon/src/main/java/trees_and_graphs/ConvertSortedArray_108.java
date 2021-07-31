28e61dd1f4b66a1c1fadccab0f0fa0372e2bf699
Saiful Abu <saifulabu25@gmail.com>
2021-07-30 19:13:08 -0500

    adding more stuff

--- amazon/src/main/java/trees_and_graphs/ConvertSortedArray_108.java
+++ amazon/src/main/java/trees_and_graphs/ConvertSortedArray_108.java
@@ -2,7 +2,18 @@ package trees_and_graphs;
 
 public class ConvertSortedArray_108 {
   public TreeNode sortedArrayToBST(int[] nums) {
-    return null;
+    return sortedBSTHelper(0, nums.length - 1, nums);
+  }
+
+  private TreeNode sortedBSTHelper(int left, int right, int[] vals) {
+    if(left > right) {
+      return null;
+    }
+    int middle = left + (right - left) / 2;
+    TreeNode r = new TreeNode(vals[middle]);
+    r.left = sortedBSTHelper(left, middle - 1, vals);
+    r.right = sortedBSTHelper(middle + 1, right, vals);
+    return r;
   }
 }
 
