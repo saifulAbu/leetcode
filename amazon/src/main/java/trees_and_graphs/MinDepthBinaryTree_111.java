fca48a5e58c11639125374cef7b332349d5cc8fa
Saiful Abu <saifulabu25@gmail.com>
2021-07-30 19:54:22 -0500

    addign more stuffs

---
+++ amazon/src/main/java/trees_and_graphs/MinDepthBinaryTree_111.java
@@ -0,0 +1,28 @@
+package trees_and_graphs;
+
+public class MinDepthBinaryTree_111 {
+  public int minDepth(TreeNode root) {
+      if(root == null) {
+        return 0;
+      }
+    return getNumNode(root);
+  }
+
+  private int getNumNode(TreeNode root) {
+
+    if(root.left == null && root.right == null) {
+      return 1;
+    }
+    int left = Integer.MAX_VALUE;
+    if(root.left != null) {
+      left = getNumNode(root.left);
+    }
+
+    int right = Integer.MAX_VALUE;
+    if(root.right != null) {
+      right = getNumNode(root.right);
+    }
+
+    return Math.min(left, right) + 1;
+  }
+}
