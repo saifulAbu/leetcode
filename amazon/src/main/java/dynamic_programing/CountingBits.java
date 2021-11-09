7eac38a4cf4e1ca623a0de8295f360a375378ff4
Abu, Saif <saif.abu@cerner.com>
2021-11-09 08:34:06 -0600

    the wheel in motion

--- amazon/src/main/java/dynamic_programing/CountingBits.java
+++ amazon/src/main/java/dynamic_programing/CountingBits.java
@@ -2,6 +2,26 @@ package dynamic_programing;
 
 public class CountingBits {
   public int[] countBits(int n) {
+    int [] dp = new int[n];
+    dp[0] = 0;
+    boolean filled = false;
+    int max = 1;
+    while(!filled){
+      for (int i = 0; i < max; i++) {
+        if(i + max <= n) {
+          dp[i+max] = dp[i] + 1;
+        } else {
+          filled = true;
+          break;
+        }
+      }
+      max = 2 * max;
+    }
+    return dp;
+  }
 
+  public static void main(String args[]) {
+    CountingBits cb = new CountingBits();
+    cb.countBits(2);
   }
 }
