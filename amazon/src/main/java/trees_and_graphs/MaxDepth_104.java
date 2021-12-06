85b2795d78ecce3c90f6efc851a7e1b9860bae21
Abu, Saif <saif.abu@cerner.com>
2021-12-05 19:11:44 -0600

    add more

--- amazon/src/main/java/trees_and_graphs/MaxDepth_104.java
+++ amazon/src/main/java/trees_and_graphs/MaxDepth_104.java
@@ -5,6 +5,21 @@ public class MaxDepth_104 {
     if(root == null) {
       return 0;
     }
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    return helper(root);
+  }
+
+  private int helper(TreeNode root) {
+    if(root.left == null && root.right == null) {
+      return 0;
+    }
+    int leftDepth, rightDepth;
+    leftDepth = rightDepth = 0;
+    if(root.left != null) {
+      leftDepth = helper(root.left);
+    }
+    if(root.right != null) {
+      rightDepth = helper(root.right);
+    }
+    return Integer.max(leftDepth, rightDepth) + 1;
   }
 }
