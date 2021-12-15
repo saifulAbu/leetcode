6bdb2368a5535cb23fa26e6bd0a6f2d78ec4d118
Abu, Saif <saif.abu@cerner.com>
2021-12-14 19:35:56 -0600

    add more stuffs

---
+++ amazon/src/main/java/misc/NumberOf1Bits_191.java
@@ -0,0 +1,16 @@
+package misc;
+
+public class NumberOf1Bits_191 {
+  // you need to treat n as an unsigned value
+  public int hammingWeight(int n) {
+    int count = 0;
+
+    for(int i = 0; i < 32; i++) {
+      if ((n & 1) == 1) {
+        count++;
+      }
+      n = n >> 1;
+    }
+    return count;
+  }
+}
