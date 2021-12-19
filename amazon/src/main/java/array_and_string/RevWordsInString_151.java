94ff48b8cfea913cd3c5bb986af3b8caedec7a4c
Abu, Saif <saif.abu@cerner.com>
2021-12-18 20:55:21 -0600

    mr

---
+++ amazon/src/main/java/array_and_string/RevWordsInString_151.java
@@ -0,0 +1,17 @@
+package array_and_string;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class RevWordsInString_151 {
+  static public String reverseWords(String s) {
+    List<String> list = Arrays.asList(s.split("\\s+"));
+    Collections.reverse(list);
+    return String.join(" ", list);
+  }
+
+  public static void main(String [] args) {
+    reverseWords("hello  ");
+  }
+}
