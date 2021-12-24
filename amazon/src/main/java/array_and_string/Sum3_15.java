e8b9561274e5dcce9f53d078e67c3478a911c6e6
Abu, Saif <saif.abu@cerner.com>
2021-12-24 10:14:06 -0600

    add more

--- amazon/src/main/java/array_and_string/Sum3_15.java
+++ amazon/src/main/java/array_and_string/Sum3_15.java
@@ -9,6 +9,39 @@ public class Sum3_15 {
       return res;
     }
     Arrays.sort(nums);
+    int p0 = 0;
+    int len = nums.length;
+    while (p0 < len - 2) {
+      int p1 = p0 + 1;
+      int p2 = len - 1;
+      while (p1 < p2) {
+        int sum = nums[p0] + nums[p1] + nums[p2];
+        if (sum == 0) {
+          List<Integer> list = Arrays.asList(nums[p0], nums[p1], nums[p2]);
+          res.add(list);
+          do {
+            p1++;
+          }  while (p1 < len - 1 && nums[p1] == nums[p1 - 1]);
+        } else if (sum < 0){
+          p1++;
+        } else {
+          p2--;
+        }
+
+      }
+      do {
+        p0++;
+      } while (p0 < len - 2 && nums[p0] == nums[p0 - 1]);
+    }
+    return res;
+  }
+
+  public List<List<Integer>> threeSum1(int[] nums) {
+    List<List<Integer>> res = new LinkedList<>();
+    if (nums.length < 3) {
+      return res;
+    }
+    Arrays.sort(nums);
     //special case 3 zeros
     int zeroCount = 0;
     for (int i : nums) {
