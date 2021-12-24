594bc9f6dcdbe7ffdccdd436f1a58d7e259f604c
Abu, Saif <saif.abu@cerner.com>
2021-12-24 17:19:34 -0600

    add kmp

--- amazon/src/main/java/array_and_string/ImplementStrStr_28.java
+++ amazon/src/main/java/array_and_string/ImplementStrStr_28.java
@@ -59,4 +59,68 @@ public class ImplementStrStr_28 {
     }
     return lps;
   }
+
+  /// new attempt
+  int [] calculateSuffixPrefixArr(String str) {
+    int [] lsp = new int[str.length()];
+    char [] arr = str.toCharArray();
+    int len = 0;
+    lsp[0] = 0;
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] == arr[len]) {
+        len++;
+        lsp[i] = len;
+      } else {
+        while (len > 0) {
+          len = lsp[len - 1];
+          if (arr[i] == arr[len]) {
+            len++;
+            lsp[i] = len;
+            break;
+          }
+        }
+        if (len == 0) {
+          lsp[i] = 0;
+          if (arr[i] == arr[len]) {
+            len++;
+            lsp[i] = len;
+          }
+        }
+      }
+    }
+    return lsp;
+  }
+
+  private int kmpSearchNew(String h, String n) {
+    int [] lsp = calculateSuffixPrefixArr(n);
+    int j = 0; //search string index
+    int i = 0;
+    char [] harr = h.toCharArray();
+    char [] narr = n.toCharArray();
+    for (; i < harr.length; i++) {
+      if (harr[i] == narr[j]) {
+        j++;
+      } else {
+        while (j > 0) {
+          j = lsp[j-1];
+          if (harr[i] == narr[j]) {
+            j++;
+            break;
+          }
+        }
+        if (j == 0 && harr[i] == narr[j]) {
+          j++;
+        }
+      }
+      if(j == narr.length) {
+        break;
+      }
+    }
+    if (j == narr.length) {
+      return i - j + 1;
+    } else {
+      return -1;
+    }
+  }
+
 }
