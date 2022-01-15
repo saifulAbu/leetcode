2875ef25e334e457f255e067f9571cad38cbeef2
Abu, Saif <saif.abu@cerner.com>
2022-01-15 13:38:52 -0600

    add more

---
+++ amazon/src/main/java/binary_search/BinarySearch_704.java
@@ -0,0 +1,18 @@
+package binary_search;
+
+public class BinarySearch_704 {
+  public int search(int[] nums, int target) {
+    int l = 0, r = nums.length - 1;
+    while (l <= r) {
+      int m = l + (r - l) / 2;
+      if (nums[m] == target) {
+        return m;
+      } else if (nums[m] < target) {
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
+    }
+    return -1;
+  }
+}
