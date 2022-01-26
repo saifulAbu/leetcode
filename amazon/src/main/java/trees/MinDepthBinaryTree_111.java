afdac0a9cedca409169c7a64771925257c6b0627
Abu, Saif <saif.abu@cerner.com>
2022-01-25 22:35:46 -0600

    add more

--- amazon/src/main/java/trees/MinDepthBinaryTree_111.java
+++ amazon/src/main/java/trees/MinDepthBinaryTree_111.java
@@ -1,6 +1,21 @@
-package trees_and_graphs;
+package trees;
 
 public class MinDepthBinaryTree_111 {
+  private class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() { }
+    TreeNode(int val) {
+      this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  };
+
   public int minDepth(TreeNode root) {
       if(root == null) {
         return 0;
