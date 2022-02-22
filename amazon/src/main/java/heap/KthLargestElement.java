2a5841e2478a14770dcfdd3318c9c213968ee49c
Abu, Saif <saif.abu@cerner.com>
2022-02-21 23:52:37 -0600

    add more

---
+++ amazon/src/main/java/heap/KthLargestElement.java
@@ -0,0 +1,19 @@
+package heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+  public int findKthLargest(int[] nums, int k) {
+    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    for (int num : nums) {
+      pq.add(num);
+    }
+
+    for (int i = 0; i < k - 1; i++) {
+      pq.remove();
+    }
+
+    return pq.peek();
+  }
+}
