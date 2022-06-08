961ed20a1132bf2bd7c223e7c048188a3dd12699
Saiful Abu <saifulabu25@gmail.com>
2022-06-08 09:19:42 -0500

    add one more

--- amazon/src/main/java/hash/FindDuplicateSubtrees_652.java
+++ amazon/src/main/java/hash/FindDuplicateSubtrees_652.java
@@ -1,5 +1,8 @@
 package hash;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class FindDuplicateSubtrees_652 {
@@ -15,8 +18,43 @@ public class FindDuplicateSubtrees_652 {
           this.right = right;
       }
   }
-  public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
 
-    return null;
+  HashMap<String, List<TreeNode>> map = new HashMap<>();
+
+  public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+    populateHashMap(root);
+    return filterList();
+  }
+
+  List<TreeNode> filterList() {
+    List<TreeNode> res = new LinkedList<>();
+    for(String key : map.keySet()) {
+      if(map.get(key).size() > 1) {
+        res.add(map.get(key).get(0));
+      }
+    }
+    return res;
+  }
+
+  String populateHashMap(TreeNode root) {
+    String hash = "";
+    if (root.left == null && root.right == null) {
+      hash = hash + root.val;
+    } else {
+      if (root.left != null) {
+        hash = populateHashMap(root.left);
+      }
+
+      hash = hash + root.val;
+
+      if (root.right != null) {
+        hash = hash + populateHashMap(root.right);
+      }
+    }
+
+    hash = "(" + hash + ")";
+    map.putIfAbsent(hash, new LinkedList<>());
+    map.get(hash).add(root);
+    return hash;
   }
 }
