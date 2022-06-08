edb1d314bd4e246482401d3a86ac042060ef6958
Saiful Abu <saifulabu25@gmail.com>
2022-06-08 08:11:34 -0500

    add more stuffs

---
+++ amazon/src/main/java/hash/FindDuplicateSubtrees_652.java
@@ -0,0 +1,22 @@
+package hash;
+
+import java.util.List;
+
+public class FindDuplicateSubtrees_652 {
+  static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+  public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+
+    return null;
+  }
+}
