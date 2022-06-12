0df17a8399b9ef220349df6364f69bd4559d11a1
Saiful Abu <saifulabu25@gmail.com>
2022-06-11 23:45:15 -0500

    add more

--- amazon/src/main/java/hash/UniqueWordAbbr_288.java
+++ amazon/src/main/java/hash/UniqueWordAbbr_288.java
@@ -1,11 +1,32 @@
 package hash;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class UniqueWordAbbr_288 {
   public UniqueWordAbbr_288(String[] dictionary) {
+    initialize(dictionary);
+  }
 
+  HashMap<String, Integer> map = new HashMap<>();
+  HashSet<String> set = new HashSet<>();
+
+  private void initialize(String[] dictionary) {
+    for(String word : dictionary) {
+      if(set.contains(word)) {
+        continue;
+      }
+      set.add(word);
+      String abr = getAbbr(word);
+      map.put(abr, map.getOrDefault(abr, 0) + 1);
+    }
   }
 
   public boolean isUnique(String word) {
+    String abr = getAbbr(word);
+    if(!map.containsKey(abr) || (set.contains(word) && map.get(abr) == 1)) {
+      return true;
+    }
     return false;
   }
 
