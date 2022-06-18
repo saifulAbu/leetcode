3d222ba0354fbb190fdf8b9962f13d1d79e88435
Saiful Abu <saifulabu25@gmail.com>
2022-06-17 23:03:00 -0500

    add more stuff

---
+++ amazon/src/main/java/trie/DesignSearchAutoCompletion_642.java
@@ -0,0 +1,13 @@
+package trie;
+
+import java.util.List;
+
+public class AutocompleteSystem_642 {
+  public AutocompleteSystem_642(String[] sentences, int[] times) {
+
+  }
+
+  public List<String> input(char c) {
+
+  }
+}
