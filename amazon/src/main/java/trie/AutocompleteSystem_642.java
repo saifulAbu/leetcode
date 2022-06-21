07822425c6788ab0dd736f02f4ef521e67b056b3
Saiful Abu <saifulabu25@gmail.com>
2022-06-20 22:03:40 -0500

    add more

--- amazon/src/main/java/trie/AutocompleteSystem_642.java
+++ amazon/src/main/java/trie/AutocompleteSystem_642.java
@@ -3,6 +3,10 @@ package trie;
 import java.util.*;
 
 public class AutocompleteSystem_642 {
+  TrieNode root = new TrieNode();
+  TrieNode curRoot = root;
+  StringBuilder wordSoFar = new StringBuilder();
+
   static class TrieNode {
     HashMap<Character, TrieNode> map = new HashMap<>();
     boolean isWord = false;
@@ -28,7 +32,7 @@ public class AutocompleteSystem_642 {
     }
   }
 
-  TrieNode root = new TrieNode();
+
   public AutocompleteSystem_642(String[] sentences, int[] times) {
     for(int i = 0; i < sentences.length; i++) {
       insert(sentences[i], times[i]);
@@ -58,16 +62,53 @@ public class AutocompleteSystem_642 {
   }
 
   public List<String> input(char c) {
-    return null;
+    List<String> strList = new LinkedList<>();
+    if(c == '#') {
+      curRoot.sentence = wordSoFar.toString();
+      curRoot.count += 1;
+      curRoot.isWord = true;
+
+      //reset
+      curRoot = root;
+      wordSoFar.setLength(0);
+      return strList;
+    }
+
+    wordSoFar.append(c);
+    curRoot.map.putIfAbsent(c, new TrieNode());
+    curRoot = curRoot.map.get(c);
+
+    List<ListElem> sentenceCounts = new LinkedList<ListElem>();
+    getSentenceCount(curRoot, sentenceCounts);
+    Collections.sort(sentenceCounts);
+
+    for(int i = 0; i < 3 && !sentenceCounts.isEmpty(); i++) {
+      strList.add(sentenceCounts.get(0).sentence);
+      sentenceCounts.remove(0);
+    }
+
+    return strList;
   }
 
   public static void main(String args[]) {
     String[] sentences = {"aaa", "aa", "a", "aba"};
-    int[] count = {7, 3, 2, 7};
-    AutocompleteSystem_642 autoComplete = new AutocompleteSystem_642(sentences, count);
-    List<ListElem> items = new LinkedList<ListElem>();
-    autoComplete.getSentenceCount(autoComplete.root, items);
-    Collections.sort(items);
+    int[] count = {5, 3, 2, 7};
+    AutocompleteSystem_642 obj = new AutocompleteSystem_642(sentences, count);
+    //List<ListElem> items = new LinkedList<ListElem>();
+    //autoComplete.getSentenceCount(autoComplete.root, items);
+    //Collections.sort(items);
+
+    List<String> list;
+    list = obj.input('a'); // return ["i love you", "island", "i love leetcode"]. There are four sentences that have prefix "i". Among them, "ironman" and "i love leetcode" have same hot degree. Since ' ' has ASCII code 32 and 'r' has ASCII code 114, "i love leetcode" should be in front of "ironman". Also we only need to output top 3 hot sentences, so "ironman" will be ignored.
+    list = obj.input(' '); // return ["i love you", "i love leetcode"]. There are only two sentences that have prefix "i ".
+    list = obj.input('a'); // return []. There are no sentences that have prefix "i a".
+    list = obj.input('#');
+
+    list = obj.input('a'); // return ["i love you", "island", "i love leetcode"]. There are four sentences that have prefix "i". Among them, "ironman" and "i love leetcode" have same hot degree. Since ' ' has ASCII code 32 and 'r' has ASCII code 114, "i love leetcode" should be in front of "ironman". Also we only need to output top 3 hot sentences, so "ironman" will be ignored.
+    list = obj.input(' '); // return ["i love you", "i love leetcode"]. There are only two sentences that have prefix "i ".
+    list = obj.input('a'); // return []. There are no sentences that have prefix "i a".
+
+
     System.out.println();
   }
 }
